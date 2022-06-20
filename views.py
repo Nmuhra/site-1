88e0fdd2105a36f7bd654f24e87d44569b218c88
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-06-20 19:19:21 +0000

    html

--- views.py
+++ views.py
@@ -1,8 +1,8 @@
-import Blueprint
+import Blueprint, render_template
 
-views = Blueprint("views")
+views = Blueprint(__name__, "views")
 
 
 @views.route("/")
 def home():
-  return "home page"
+  return render_template("index.html")
